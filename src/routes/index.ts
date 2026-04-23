c5bdcf53b5f2af97ad7b7ebe8a810031788453c8
Arjun-Bhaskar <110147729+Arjun-Bhaskar@users.noreply.github.com>
2026-04-23 20:56:50 +1000

    fixed the routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import { healthRoutes } from "../modules/health";
+import userRoutes from "../modules/user-management";
 
 const router = Router();
 
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 
 router.use("/auth", authRoutes);
 
