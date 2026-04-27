1916b3c4c7dc44423cf257f4b5fde679a917696c
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2026-04-27 10:21:49 +1000

    feat: project tree types apis added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import { healthRoutes } from "../modules/health";
+import { projectTreeTypesRoutes } from "../modules/project-tree-types";
 import { treeTypesRoutes } from "../modules/tree-types";
 
 const router = Router();
@@ -8,5 +9,6 @@ const router = Router();
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/tree-types", treeTypesRoutes);
+router.use("/project-tree-types", projectTreeTypesRoutes);
 
 export default router;
