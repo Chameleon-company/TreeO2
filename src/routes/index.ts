cec058c817cb9838d1a1c907bcb3e96309e45ff3
Sandali Samarawickrama <wathmali123@gmail.com>
2026-05-02 21:53:54 +1000

    remove unnecessary blank lines in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,11 @@ import { healthRoutes } from "../modules/health";
 import { projectManagementRoutes } from "../modules/project-management";
 import { localizationRoutes } from "../modules/localization";
 
-
 const router = Router();
 
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/projects", projectManagementRoutes);
 router.use("/localized-strings", localizationRoutes);
-
 
 export default router;
