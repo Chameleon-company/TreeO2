792a3695ea4d1e9ec336b5f5a2b2a650109baa9a
AKV2703 <s223524319@deakin.edu.au>
2026-05-04 17:14:19 +1000

    Refactor import statements for auth and user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { authRoutes } from "../modules/auth/auth.routes";
+import authRoutes from "../modules/auth/auth.routes";
 import { healthRoutes } from "../modules/health";
-import { userRoutes } from "../modules/user-management";
+import userRoutes from "../modules/user-management";
 import { treeTypesRoutes } from "../modules/tree-types";
 import { projectManagementRoutes } from "../modules/project-management";
 import { localizationRoutes } from "../modules/localization";
