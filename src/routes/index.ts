bb4335a9ae78690b1ec4984ff1ceb3fc1c80a4e0
Jaykumar677 <jai.jk739@gmail.com>
2026-05-16 02:54:40 +1000

    feat(scan-batches): register routes and module exports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,7 @@ import { localizationRoutes } from "../modules/localization";
 import { adoptersRouter } from "../modules/adopters";
 import { userProjectAssignmentRoutes } from "../modules/user-project-assignment";
 import { partnersRoutes } from "../modules/partners";
+import { scanBatchesRoutes } from "../modules/scan-batches";
 
 import treeScansRoutes from "../modules/tree-scans";
 
@@ -24,6 +25,7 @@ router.use("/localized-strings", localizationRoutes);
 router.use("/user-projects", userProjectAssignmentRoutes);
 router.use("/project-tree-types", projectTreeTypesRoutes);
 router.use("/partners", partnersRoutes);
+router.use("/scan-batches", scanBatchesRoutes);
 
 router.use("/tree-scans", treeScansRoutes);
 
