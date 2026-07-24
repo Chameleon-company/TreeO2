69e069edadc79676aa7af1242e471107c49d4676
ponleou <dev.ponleousk@gmail.com>
2026-07-24 23:29:14 +1000

    ci: fix formatting config and added linting for prisma and test scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
     ecmaVersion: 2020,
     sourceType: 'module',
   },
@@ -35,5 +35,21 @@ module.exports = {
     // --- Imports ---
     'no-duplicate-imports': 'error',
   },
+  // ignoring specific lints for tests for simplification
+  // context: lint for tests were added later after 471 tests already exists, so its not worth fixing these lints
+  overrides: [
+    {
+      files: ['tests/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+      },
+    },
+  ],
   ignorePatterns: ['dist/', 'node_modules/', 'jest.config.js', '*.js'],
 };
